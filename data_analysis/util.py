d3b6f9bdb181c298f370a3f8badf0a2d8c1cd0c6
Rucha Dave <ruchadave16@gmail.com>
2024-12-02 15:39:10 -0500

    Add filtering function

--- data_analysis/util.py
+++ data_analysis/util.py
@@ -10,7 +10,7 @@ def timestamp_split(file_path):
     Split timestamp columns into date and time columns
 
     Args:
-        file_path: string representing the path to the csv file to be split
+        file_path (str): string representing the path to the csv file to be split
 
     Returns a pandas dataframe with new columns of "date" and "time" instead of the "timestamp"
     """
@@ -19,3 +19,20 @@ def timestamp_split(file_path):
     df["datetime"] = pd.to_datetime(df["datetime"], format="%Y-%m-%d %H:%M:%S")
     df = df.drop(columns="timestamp")
     return df
+
+
+def filter_setpoint(df):
+    """
+    Filter data so that only points where the data is in between the high and low thresholds is
+    kept. This runs on the assumption that unoccupied rooms usually end up at one of the extremes
+
+    Args:
+        df (pandas.DataFrame): dataframe containing data to filter
+
+    Returns a filtered pandas DataFrame with only rows that are "occupied"
+    """
+    top_threshold = max(df["RmTmpCspt"])
+    bottom_threshold = min(df["RmTmpHpst"])
+    df_filtered = df[df["RmTmpCspt"] < top_threshold]
+    df_filtered = df_filtered[df["RmTmpHpst"] > bottom_threshold]
+    return df_filtered
